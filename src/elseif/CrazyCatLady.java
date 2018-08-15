6b53e5b3be61f46faa47508b4e5a2667bde1d825
UltimatePrograms <league@WTS-iMac-11.attlocal.net>
2018-08-15 11:53:16 -0700

    day 7 of summerslam

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,18 +5,28 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String cat = JOptionPane.showInputDialog("how many cats do you have?");
 		// 2. Convert their answer into an int
-
+int c = Integer.parseInt(cat);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+if (c >= 3) {
+	JOptionPane.showMessageDialog(null, "you are a crazy cat lady.");
+}
+else
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+if (c <=3 && c>0) {
+	playVideo("https://www.bing.com/videos/search?q=cat+videos+clean&view=detail&mid=C72DEA898EFA16A29845C72DEA898EFA16A29845&FORM=VIRE");
+}
+else
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
+		if (c < 1) {
+			playVideo("https://www.youtube.com/watch?v=oj_yLBltPE8");
+		}
 	}
 
 	static void playVideo(String videoURL) {
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,24 +4,70 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
-		
-		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
+		// 1. Create a new Robot
+		Robot bb = new Robot();
+		bb.hide();
+		bb.penDown();
+		bb.setSpeed(999);
+		// 3. Ask the user what color they would like the robot to draw
+		for (int r= 0; r < 100; r++) {
+			String color = JOptionPane.showInputDialog("Enter color from RBG");
+			// 5. Use an if/else statement to set the pen color that the user requested
+			if (color.equalsIgnoreCase("red")) {
+				bb.setPenColor(Color.red);
+			} else if (color.equalsIgnoreCase("blue")) {
+				bb.setPenColor(Color.blue);
+			} else if (color.equalsIgnoreCase("green")) {
+				bb.setPenColor(Color.green);
+			}
+			// 6. If the user doesn’t enter anything, choose a random color
+			else {
+				JOptionPane.showMessageDialog(null, "We don't have that");
+				bb.setRandomPenColor();
+			}
+			// 7. Put a loop around your code so that you keep asking the user for more
+			// colors & drawing them
 
-        //6. If the user doesn’t enter anything, choose a random color
+			// 4. Set the pen width to 10
+			bb.setPenWidth(1);
+			// 2. Make the robot draw a shape (this will take more than one line of code)
 
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
+			int numSides = 5;
+			// 5. Make a second variable for the angle you want the robot to turn. Hint: you
+			// can divide in Java using "/". Can’t test until step 6
+			int angle = 360 / numSides;
+			// 7. Use a for loop to repeat steps #2 to #11, 200 times. When this is done you
+			// should see a pentagon
+			for (int q = 0; q< 999;q++) {
+				// 2. Move the robot 200 pixels
 		
-		//4. Set the pen width to 10
+				// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
 		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
+				bb.move(q);
+				// 10. Change the previous line of code to make the robot move "i" pixels
+				// instead of 200
 
+				// 6. Turn the robot the amount in your angle variable
+		
+				// 11. Turn the robot one more degree
+				bb.turn(angle);
+
+				// 11. Turn the robot one more degree
+	bb.turn(1);
+			}
+		}
+		}
+	
+
+	private static void drawSquare() {
+		// TODO Auto-generated method stub
 
 	}
 }
